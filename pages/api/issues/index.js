18de1228cca2f3b59633af7fb9256b7b22bed88b
Mrinmay Santra <mrinmay7875@gmail.com>
2022-07-23 13:47:23 +0530

    Initial Commit

---
+++ pages/api/issues/index.js
@@ -0,0 +1,18 @@
+import { Octokit } from "octokit";
+
+export default async function handler(req, res) {
+  const octokit = new Octokit();
+
+  const q = "is:open is:issue label:good-first-issue";
+
+  const response = await octokit.request("GET /search/issues", { q });
+  const results = response.data.items.map((item) => ({
+    name: item.title,
+    author: item.user.login,
+    labels: item.labels.map((label) => label.name),
+    url: item.html_url,
+  }));
+
+  const random = Math.floor(Math.random() * results.length + 1);
+  res.status(200).json(results[random]);
+}
